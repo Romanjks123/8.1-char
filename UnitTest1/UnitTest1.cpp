da2affe2bf3cff13de421ad998d64cc730f83788
Romanjks123 <m7playyt@gmail.com>
2024-10-30 22:29:40 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../8.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abcabcabc";
+			int res;
+			res = Count(str);
+			Assert::AreEqual(res, 3);
+		}
+
+	};
+}
